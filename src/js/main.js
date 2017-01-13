f6ec9fb4048d3a5071564e7a8367cbd060d22552
Mallerie Shirley <mallerie@gmail.com>
2017-01-13 00:03:02 -0500

    Added home controller to registry

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 import angular from 'angular';
+
+import { routerConfig } from './routes';
+
+import { HomeController } from './controllers/home';
 import 'angular-ui-router';
 
 angular
-  .module('app'['ui.router'])
+  .module('app', ['ui.router'])
   .config(routerConfig)
   .controller('HomeController', HomeController)
   ;
