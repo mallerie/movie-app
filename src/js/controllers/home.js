1510676da4ae3e76891226e6bfb517c67285a113
Mallerie Shirley <mallerie@gmail.com>
2017-01-14 12:15:23 -0500

    Added movie service and succesfully hit the server

--- src/js/controllers/home.js
+++ src/js/controllers/home.js
@@ -1,14 +1,34 @@
-function HomeController () {
+import { SERVER } from "../server";
+console.log(SERVER);
+
+function HomeController (MovieService, $http) {
 
   let vm = this;
 
+  vm.movies = [];
+
   function init () {
     console.log("Home Controller Ran");
+
+    MovieService.allMovies().then((resp) => {
+      vm.movies = resp.data.Search;
+      console.log(vm.movies);
+    });
+
+    // $http({
+    //   method: 'GET',
+    //   url: SERVER,
+    // }).then((resp) => {
+    //   console.log(resp.data);
+    //   });
+
   }
 
   init();
 
+
 }
 
+HomeController.$inject = ['MovieService','$http'];
 export { HomeController };
 
