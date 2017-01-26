1664fd174362f284a978a12ccc14819d1bd19fd7
Mallerie Shirley <mallerie@gmail.com>
2017-01-26 09:32:02 -0500

    Cleaned up code

--- src/js/services/movieservice.js
+++ src/js/services/movieservice.js
@@ -3,13 +3,11 @@ import { SERVER } from "../server";
 function MovieService ($http) {
 
   this.searchMovies = function (query) {
-    console.log(query, "<----This is the query");
     let url = SERVER + "&s=" + query;
     return $http.get(url);
   };
 
   this.initMovies = function () {
-    // console.log(query, "<----This is the query");
     let url = SERVER + "&s=Star+Wars";
     return $http.get(url);
   };
@@ -20,7 +18,7 @@ function MovieService ($http) {
   };
 
   this.getFavorites = function () {
-    let url = "https://movie-app-96200.firebaseio.com/favorites";
+    let url = "https://movie-app-96200.firebaseio.com/";
     return $http.get(url);
   }
 
